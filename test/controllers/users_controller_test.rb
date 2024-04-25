3129f12bd9930e5414c08526be5364cc51017a3a
Glenn Goodrich <glenn.goodrich@method.com>
2024-04-25 15:36:43 +0200

    add password confirmation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,7 +10,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
         user: {
           name: 'John',
           email: 'john@example.com',
-          password: 'password'
+          password: 'password',
+          password_confirmation: 'password'
         }
       }
     end
@@ -28,11 +29,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
         user: {
           name: 'John',
           email: 'johndoe@example.com',
-          password: 'pass'
+          password: 'pass',
+          password_confirmation: 'pass'
         }
       }
     end
     assert_response :unprocessable_entity
     assert_select 'p.is-danger', text: I18n.t('activerecord.errors.models.user.attributes.password.too_short')
+  end
+
+  test "renders errors if passwords don't match" do
+    get sign_up_path
+    assert_response :ok
+    assert_no_difference ['User.count', 'Organization.count'] do
+      post sign_up_path, params: {
+        user: {
+          name: 'John',
+          email: 'johndoe@example.com',
+          password: 'password',
+          password_confirmation: 'assaadfa'
+        }
+      }
+    end
+    assert_response :unprocessable_entity
+    assert_select 'p.is-danger',
+                  text: I18n.t('activerecord.errors.models.user.attributes.password_confirmation.confirmation')
   end
 end
