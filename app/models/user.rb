282184b06477a455fb6ed9af421abedcd1ecf8a5
Glenn Goodrich <glenn.goodrich@method.com>
2024-04-26 10:26:43 +0200

    Refactor user model with authenication concern and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,28 +2,15 @@
 
 # user class
 class User < ApplicationRecord
-  has_secure_password
+  include Authentication
   validates :name, presence: true
   validates :email,
             format: { with: URI::MailTo::EMAIL_REGEXP },
             uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }
 
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
-  has_many :app_sessions
 
   normalizes :name, with: ->(name) { name.strip }
   normalizes :email, with: ->(email) { email.strip.downcase }
-
-  def self.create_app_session(email:, password:)
-    user = User.authenticate_by(email:, password:)
-    user.app_sessions.create if user.present?
-  end
-
-  def authenticate_app_session(app_session_id, token)
-    app_sessions.find(app_session_id).authenticate_token(token)
-  rescue ActiveRecord::RecordNotFound
-    nil
-  end
 end
